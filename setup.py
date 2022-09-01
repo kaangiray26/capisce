78437b109aec227da2d783f479205350939e488c
Kaan Giray Buzluk <kaangiray26@protonmail.com>
2022-09-01 17:34:42 +0200

    v0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='capisce',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_namespace_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True
